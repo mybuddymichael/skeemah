f7dace7a21b7ae219c8326a4707f955675eca6d2
Michael Hanson <michael.b.hanson@gmail.com>
2011-11-20 21:10:29 -0800

    Caching not needed for 'current_user'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
 
   helper_method :current_user
@@ -6,7 +7,7 @@ class ApplicationController < ActionController::Base
 private
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    User.find(session[:user_id]) if session[:user_id]
   end
 
 end
