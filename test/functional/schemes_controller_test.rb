8e029786ff8fad8bbe851fe28acb8253e0721d0e
Michael Hanson <michael.b.hanson@gmail.com>
2011-11-22 14:19:57 -0800

    Change some tests for schemes controller

--- test/functional/schemes_controller_test.rb
+++ test/functional/schemes_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class SchemesControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
+  test 'redirect to splash if not logged in' do
+    get :new, {}, {id: nil}
+    assert_redirected_to :root
+  end
+
+  test 'gets new if logged in' do
+    get :new, {}, {id: true}
     assert_response :success
   end
 
