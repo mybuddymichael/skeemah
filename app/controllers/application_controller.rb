60a4818de03c24107d23a69f19e24c4e63b7cb90
Michael Hanson <michael.b.hanson@gmail.com>
2011-11-22 14:17:44 -0800

    Add logged_in?, change session[:user_id] to session[:id]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,22 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
 private
 
+  def authenticate
+    unless logged_in?
+      redirect_to :root
+    end
+  end
+
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    User.find(session[:id]) if session[:id]
+  end
+
+  def logged_in?
+    session[:id]
   end
 
 end
