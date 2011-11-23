82ad6837dddb8a07ac90989be7ffcfc652f434a2
Michael Hanson <michael.b.hanson@gmail.com>
2011-11-22 22:34:48 -0800

    ENV

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, 'b2dc37d28144c5876735', '4af98d2148ffd041a1df68c5409088024ab2fc55'
+  provider :github, ENV['GITHUB_ID'], ENV['GITHUB_ID']
 end
