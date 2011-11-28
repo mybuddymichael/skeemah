55f9225e5f1d65dba8679ee7acc3c9b815716caf
Michael Hanson <michael.b.hanson@gmail.com>
2011-11-28 15:51:46 -0800

    Fix SchemesController test

--- test/functional/schemes_controller_test.rb
+++ test/functional/schemes_controller_test.rb
@@ -1,13 +1,14 @@
 require 'test_helper'
 
 class SchemesControllerTest < ActionController::TestCase
+
   test 'redirect to splash if not logged in' do
     get :new, {}, {id: nil}
     assert_redirected_to :root
   end
 
   test 'gets new if logged in' do
-    get :new, {}, {id: true}
+    get :new, {}, {id: 1}
     assert_response :success
   end
 
