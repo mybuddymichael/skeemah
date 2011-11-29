18245967a2e2503193c36aa7e644ae8133590307
Michael Hanson <michael.b.hanson@gmail.com>
2011-11-29 13:19:39 -0800

    Add DashboardHelper::IndexView

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,13 @@
 module DashboardHelper
+  class IndexView < LayoutHelper::ApplicationView
+
+    def username
+      @current_user.username
+    end
+
+    def no_schemes
+      @current_user.schemes.empty?
+    end
+
+  end
 end
