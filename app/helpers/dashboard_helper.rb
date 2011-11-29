6c4e0ef47392e878c28dff1981d492743e45e78c
Michael Hanson <michael.b.hanson@gmail.com>
2011-11-29 13:42:10 -0800

    Document DashboardHelper::IndexView.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,10 +1,13 @@
 module DashboardHelper
+  # The view for the dashboard/index template.
   class IndexView < LayoutHelper::ApplicationView
 
+    # Returns a string of the current user's username.
     def username
       @current_user.username
     end
 
+    # Returns true if the current user has not created or forked any schemes.
     def no_schemes
       @current_user.schemes.empty?
     end
