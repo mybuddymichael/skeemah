d5f306ed8e50e3c46687216ea87d49c7cc8dd322
Michael Hanson <michael.b.hanson@gmail.com>
2011-11-29 00:11:36 -0800

    Document LayoutHelper::MainLayoutView

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,11 +1,16 @@
 module LayoutHelper
+  # MainLayoutView is the application's layout view, corresponding to
+  # views/layouts/application.html.erb.
   class MainLayoutView
 
-    attr_reader :current_user
-
+    # Initialize the view and pass along the current_user, which is in
+    # ApplicationController.
     def initialize(current_user)
       @current_user = current_user
     end
+
+    # Returns the logged-in user, or nil if none.
+    attr_reader :current_user
 
   end
 end
